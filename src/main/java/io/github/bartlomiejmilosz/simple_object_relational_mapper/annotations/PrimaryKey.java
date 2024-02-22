b47b1487102c864032cf6ca7131089b3ff52b492
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-22 18:04:05 +0100

    Add Person model and annotate fields by created @metadata

---
+++ src/main/java/io/github/bartlomiejmilosz/simple_object_relational_mapper/annotations/PrimaryKey.java
@@ -0,0 +1,8 @@
+package io.github.bartlomiejmilosz.simple_object_relational_mapper.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrimaryKey {
+}
