01708882642348d8ce4a5a4d699eda4661bf1d50
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-23 20:13:31 +0100

    Add definition of column and primary key fields

--- src/main/java/io/github/bartlomiejmilosz/simple_object_relational_mapper/util/ColumnField.java
+++ src/main/java/io/github/bartlomiejmilosz/simple_object_relational_mapper/util/ColumnField.java
@@ -2,18 +2,26 @@ package io.github.bartlomiejmilosz.simple_object_relational_mapper.util;
 
 import java.lang.reflect.Field;
 
+import io.github.bartlomiejmilosz.simple_object_relational_mapper.annotations.Column;
+
 public class ColumnField {
     private Field field;
+	private Column column;
 
-    public ColumnField(Field field) {
-        this.field = field;
-    }
+	public ColumnField(Field field) {
+		this.field = field;
+		this.column = field.getAnnotation(Column.class);
+	}
 
-    public String getName() {
-        return field.getName();
-    }
+	public String getName() {
+		return column.name();
+	}
 
-    public Class<?> getType() {
-        return field.getType();
-    }
+	public Class<?> getType() {
+		return field.getType();
+	}
+
+	public Field getField() {
+		return this.field;
+	}
 }
--- src/main/java/io/github/bartlomiejmilosz/simple_object_relational_mapper/util/PrimaryKeyField.java
+++ src/main/java/io/github/bartlomiejmilosz/simple_object_relational_mapper/util/PrimaryKeyField.java
@@ -2,18 +2,26 @@ package io.github.bartlomiejmilosz.simple_object_relational_mapper.util;
 
 import java.lang.reflect.Field;
 
+import io.github.bartlomiejmilosz.simple_object_relational_mapper.annotations.PrimaryKey;
+
 public class PrimaryKeyField {
-    private Field field;
+    	private Field field;
+	private PrimaryKey primaryKey;
 
-    public PrimaryKeyField(Field field) {
-        this.field = field;
-    }
+	public PrimaryKeyField(Field field) {
+		this.field = field;
+		this.primaryKey = this.field.getAnnotation(PrimaryKey.class);
+	}
 
-    public String getName() {
-        return field.getName();
-    }
+	public String getName() {
+		return primaryKey.name();
+	}
 
-    public Class<?> getType() {
-        return field.getType();
-    }
+	public Class<?> getType() {
+		return field.getType();
+	}
+
+	public Field getField() {
+		return this.field;
+	}
 }
